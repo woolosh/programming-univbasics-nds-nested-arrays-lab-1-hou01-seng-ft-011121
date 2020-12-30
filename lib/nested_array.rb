24aa5c6118df42e71faf06e96ca3a691bb7fa3e4
Joshua Morales <joshuapmorales@gmail.com>
2020-12-30 20:50:57 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,6 +1,27 @@
 # Examples inspired by U. S. National Organic Standards
 
-ORGANIC_PRODUCE = [
+# ORGANIC_PRODUCE = [
+#   "Strawberries",
+#   "Potatoes",
+#   "Grapes",
+#   "Avocadoes",
+#   "Asparagus"
+# ]
+
+# CONVENTIONAL_PRODUCE = [
+#   "Grapefruit",
+#   "Pineapple",
+#   "Oranges",
+#   "Watermelon",
+#   "Eggplant"
+# ]
+
+def assembled_matrix
+  # Build an array that contains both of the above arrays
+  # This matrix will represent a produce storage room
+  # Organic standards require that organic products be stored ABOVE conventional, not the other way around
+  # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
+  ORGANIC_PRODUCE = [
   "Strawberries",
   "Potatoes",
   "Grapes",
@@ -15,12 +36,7 @@ CONVENTIONAL_PRODUCE = [
   "Watermelon",
   "Eggplant"
 ]
-
-def assembled_matrix
-  # Build an array that contains both of the above arrays
-  # This matrix will represent a produce storage room
-  # Organic standards require that organic products be stored ABOVE conventional, not the other way around
-  # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
+  
 end
 
 def sorted_matrix
