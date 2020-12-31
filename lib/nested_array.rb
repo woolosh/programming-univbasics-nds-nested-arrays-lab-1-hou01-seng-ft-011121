a0516f3a607e4ee1ea4b5713d391ea18bd89da38
Joshua Morales <joshuapmorales@gmail.com>
2020-12-31 19:42:41 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,27 +1,6 @@
 # Examples inspired by U. S. National Organic Standards
 
-# ORGANIC_PRODUCE = [
-#   "Strawberries",
-#   "Potatoes",
-#   "Grapes",
-#   "Avocadoes",
-#   "Asparagus"
-# ]
-
-# CONVENTIONAL_PRODUCE = [
-#   "Grapefruit",
-#   "Pineapple",
-#   "Oranges",
-#   "Watermelon",
-#   "Eggplant"
-# ]
-
-def assembled_matrix
-  # Build an array that contains both of the above arrays
-  # This matrix will represent a produce storage room
-  # Organic standards require that organic products be stored ABOVE conventional, not the other way around
-  # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
-  ORGANIC_PRODUCE = [
+ORGANIC_PRODUCE = [
   "Strawberries",
   "Potatoes",
   "Grapes",
@@ -36,22 +15,40 @@ CONVENTIONAL_PRODUCE = [
   "Watermelon",
   "Eggplant"
 ]
+
+def assembled_matrix
+  
+  produce_storage_room = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
+  
+  return produce_storage_room
+    
+end
+
+
+def sorted_matrix
+  
+  sorted_conventional_produce = CONVENTIONAL_PRODUCE.sort
+  
+  sorted_organice_produce = ORGANIC_PRODUCE.sort
+  
+  sorted_produce = [sorted_conventional_produce, sorted_organice_produce]
+  
+  return sorted_produce
   
 end
 
-def sorted_matrix
-  # Using Array literal syntax only, build another nested array that 
-  # uses the arrays of conventional and organic produce as before.
-  # However, this time, sort each internal array alphabetically by the first character
-end
 
 def matrix_lookup(matrix, row, column)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Return the matrix's content at that row and and column
+
+  return matrix[row][column]
+  
 end
 
+
 def matrix_update(matrix, row, column, new_value)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Update the matrix location at that row and column to have the value of new_value
-  # Return the updated matrix
+
+  matrix[row][column] = new_value
+  
+  return matrix
+  
 end
